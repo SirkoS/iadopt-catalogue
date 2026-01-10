cb25209ab1c6c1cee778bbf77e00666c46e38f8c
Sirko Schindler <git@sirko.org>
2026-01-10 13:16:24 +0100

    update dependencies

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -9,16 +9,53 @@ const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	...svelte.configs.recommended,
-	{
-		languageOptions: {
-			globals: { ...globals.browser, ...globals.node }
-		}
-	},
-	{
-		files: ['**/*.svelte', '**/*.svelte.js'],
-		languageOptions: { parserOptions: { svelteConfig } }
-	}
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...svelte.configs.recommended,
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node }
+    }
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.js'],
+    languageOptions: { parserOptions: { svelteConfig } },
+  }, {
+
+    rules: { // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+      // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+      'no-undef': 'off',
+
+      'indent': [
+        'warn',
+        2,
+        {
+          'VariableDeclarator': { 'var': 2, 'let': 2, 'const': 3 },
+          'SwitchCase': 1,
+          'MemberExpression': 1,
+          'CallExpression': { 'arguments': 'first' },
+          'ArrayExpression': 'first',
+          'ObjectExpression': 'first',
+          'ignoredNodes': ['ConditionalExpression']
+        },
+      ],
+      'quotes': [
+        'warn',
+        'single'
+      ],
+      'semi': [
+        'warn',
+        'always'
+      ],
+      'no-console': [
+        'warn',
+        { allow: ['warn', 'error'] }
+      ],
+      'no-trailing-spaces': [
+        'warn'
+      ]
+
+    }
+
+  }
 ];
